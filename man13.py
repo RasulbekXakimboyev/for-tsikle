1d2727fb8ca2fba0034aff702f970ba9cec67900
Rasulbek Xakimbayev <rasulbekxakimbayev@gmail.com>
2025-11-17 16:30:48 +0500

    Add files via upload

---
+++ man13.py
@@ -0,0 +1,10 @@
+friends = ["Durbek", "Farhotjohn", "Sarvarbek", "Narvozbek", "Behruzbek","Sulaymon","Murodbrk", "Lobarhon","OLtinoy"]
+
+for friend in friends:
+    print(f"")
+    if len(friend) == 8:
+        print(f"Salom! {friend}")
+    elif len(friend) == 9:
+        print(f"Asalomu alaykum {friend} ")
+    else:
+        print(f"salom!, {friend} yaxshimisan")
